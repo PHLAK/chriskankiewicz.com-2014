207e84273ac185bbc47b2adc3671cf25808c9d23
Chris Kankiewicz <Chris@ChrisKankiewicz.com>
2013-09-24 12:01:12 -0700

    Added navbar affixing on load, scroll and resize

--- public/js/chriskankiewicz.js
+++ public/js/chriskankiewicz.js
@@ -3,4 +3,29 @@ $(document).ready(function() {
     // Enable Tooltips
     $('[rel="tooltip"]').tooltip();
 
+
+    // Affix navbar on page load
+    affixNavbar();
+
+    // Affix navbar on scroll
+    $(window).scroll(function() {
+        affixNavbar();
+    });
+
+    // Affix navbar on resize
+    $(window).resize(function() {
+        affixNavbar();
+    });
+
 });
+
+
+function affixNavbar() {
+    if ($(window).scrollTop() >= $('.site-header').outerHeight()) {
+        $('.site-nav').removeClass('navbar-static-top').addClass('navbar-fixed-top');
+        $('.site-header').css('margin-bottom', $('.site-nav').outerHeight());
+    } else {
+        $('.site-nav').removeClass('navbar-fixed-top').addClass('navbar-static-top');
+        $('.site-header').css('margin-bottom', 0);
+    }
+}
